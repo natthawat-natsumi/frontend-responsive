76d1650e289f4e8737e8073344a84322d04a4e62
Natthawat Lekrath <natthawat.lek@gmail.com>
2023-07-08 22:14:33 +0700

    update layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import styled from '@emotion/styled'
 import Athlets from '../Athlets'
+import Player from '../Player'
 
 class Home extends Component {
   render = () => {
     return (
       <HTMLViewPort>
         <Athlets />
+        <HTMLBreakPoint />
+        <Player />
       </HTMLViewPort>
     )
   }
@@ -15,8 +18,17 @@ export default Home
 
 const HTMLViewPort = styled.div`
   display: block;
+  background-color: #FFFFFF;
   max-width: 1280px;
   height: 100vh;
   margin: 0px auto;
   overflow: auto;
+`
+const HTMLBreakPoint = styled.div`
+  display: block;
+  width: 100%;
+  height: 100px;
+  @media only screen and (max-width: 420px) {
+    height: 40px;
+  }
 `
--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,9 +1,8 @@
 import React, { Component, createRef } from 'react'
 import styled from '@emotion/styled'
-import FootballerPhoto from '../../images/footballer-big.svg'
-import FootballerWatermask from '../../images/footballer-watermask.svg'
+import BasketballPhoto from '../../images/basketball.svg'
 
-class Athlets extends Component {
+class Player extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -36,7 +35,7 @@ class Athlets extends Component {
     const { activeSlide } = this.state
     return (
       <HTMLContent>
-        <HTMLTitle>ATHLETS</HTMLTitle>
+        <HTMLTitle>PLAYERS</HTMLTitle>
         <HTMLPhoto />
         <HTMLList ref={this.slideRef}>
           <HTMLItemList>
@@ -45,7 +44,8 @@ class Athlets extends Component {
               <HTMLText className='_title'>CONNECTION</HTMLText>
             </HTMLSection>
             <HTMLSection className='item-content'>
-              Connect with coaches directly, you can ping coaches to view profile.
+              Connect with talented athlete directly, you can watch their 
+              skills through video showreels directly from Surface 1.
             </HTMLSection>
           </HTMLItemList>
 
@@ -55,7 +55,7 @@ class Athlets extends Component {
               <HTMLText className='_title'>COLLABORATION</HTMLText>
             </HTMLSection>
             <HTMLSection className='item-content'>
-              Work with other student athletes to  increase visability. When you share and like other players videos it will increase your visability as a player. This is the team work aspect to Surface 1.
+            Work with recruiter to increase your chances of finding talented athlete.
             </HTMLSection>
           </HTMLItemList>
 
@@ -65,7 +65,7 @@ class Athlets extends Component {
               <HTMLText className='_title'>GROWTH</HTMLText>
             </HTMLSection>
             <HTMLSection className='item-content'>
-              Resources and tools for you to get better as a student Athelte. Access to training classes, tutor sessions, etc
+              Save your time, recruit proper athlets for your team.
             </HTMLSection>
           </HTMLItemList>
           <HTMLNavigation>
@@ -78,7 +78,7 @@ class Athlets extends Component {
     )
   }
 }
-export default Athlets
+export default Player
 
 const HTMLContent = styled.div`
   display: block;
@@ -88,7 +88,7 @@ const HTMLContent = styled.div`
   .item-head {
     display: flex;
     align-items: center;
-    padding: 0px 8% 0px 50%;
+    padding: 0px 50% 0px 8%;
     ._no {
       font-size: 18px;
       font-weight: 400;
@@ -110,7 +110,7 @@ const HTMLContent = styled.div`
     }
   }
   .item-content {
-    padding: 0px 8% 0px 50%;
+    padding: 0px 50% 0px 8%;
     font-size: 20px;
     font-weight: 400;
     margin: 30px 0px 0px;
@@ -124,7 +124,7 @@ const HTMLContent = styled.div`
       height: 280px;
     }
     .item-head {
-      padding: 36px 20px 0px;
+      padding:  36px 20px 0px;
     }
     .item-content {
       padding: 0px 20px;
@@ -135,7 +135,7 @@ const HTMLContent = styled.div`
 
 const HTMLTitle = styled.h1`
   display: block;
-  padding: 0px 8% 0px 50%;
+  padding: 0px 50% 0px 8%;
   font-size: 90px;
   font-weight: 400;
   color: #E7E7E7;
@@ -168,16 +168,12 @@ const HTMLItemList = styled.li`
   padding: 26px 0px;
   &:nth-child(2) { background-color: #F5F4F9; }
   &:nth-child(3) {
-    background-color: #5E3DB3;
+    background-color: #090C35;
+    color: #8765DD;
     .item-head {
       ._no {
         &:after {
-          content: '';
-          display: block;
           background-color: #FFFFFF;
-          width: 100%;
-          height: 5px;
-          border-radius: 5px;
         }
       }
     }
@@ -203,29 +199,26 @@ const HTMLItemList = styled.li`
 `
 const HTMLPhoto = styled.div`
   display: block;
-  background-image: url(${FootballerPhoto});
+  background-image: url(${BasketballPhoto});
   background-repeat: no-repeat;
   background-size: contain;
-  background-position: calc(100% - 50px) center;
   position: absolute;
-  top: 40px;
-  left: 0px;
-  width: 50%;
+  top: -46px;
+  right: 0px;
+  width: 60%;
   height: 100%;
   transition: 1s;
-  z-index: 10;
 
   @media only screen and (max-width: 420px) {
-    background-position: center!important;
-    width: 100%!important;
-    height: 250px;
-    top: 60px!important;
-    left: 0px!important;
+    background-position: center !important;
+    width: 100%  !important;
+    height: 240px  !important;
+    top: 80px  !important;
+    left: 0px !important;
   }
 
   @media only screen and (max-width: 768px) {
-    background-image: url(${FootballerWatermask});
-    background-position: calc(100% - 180px) center;
+    background-position: 120px calc(100% - 195px);
     width: 70%;
   }
 `
